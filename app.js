f98c637f553abf6ac2849eb72d110ff3b162fa59
Yassin <81421290+Yacheen@users.noreply.github.com>
2022-02-05 22:52:30 -0700

    initial commit of Bug's bot

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const { Client } = require("discord.js");
+
+const client = new Client({
+    intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_TYPING"],
+});
+
+const PREFIX = "!";
+const cooldown = 60 * 20;
+
+client.login(process.env.BOT_TOKEN);
+
+client.on("ready", (c) => {
+    console.log(`${c.user.tag} is ready.`);
+
+    client.user.setPresence({
+        activities: [{ name: "GenJhin ipact xd", type: "PLAYING" }],
+        status: "online",
+    });
+});
